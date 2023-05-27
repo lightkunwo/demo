bc947c9036f2c8bdfd3ec5e0328e27f258f4dbba
fhh <2767203938@qq.com>
2023-05-27 19:31:16 +0800

    提交文件

---
+++ javabook01.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;//调用这个方法
+public class javabook01 {
+    public static void main(String[] args){
+        /*计算圆的面积
+        double radius;
+        double area;
+        Scanner input=new Scanner(System.in);
+        radius=input.nextDouble();
+        area=radius*radius*3.14159;
+        System.out.println("The area is "+area);
+    */
+        /*求三个数的平均数
+        Scanner in=new Scanner(System.in);
+        double x=in.nextDouble();
+        double y=in.nextDouble();
+        double z=in.nextDouble();
+                double average=(x+y+z)/3.0;
+        System.out.println("平均数为:"+average);
+*/
+        /*整数简单的加减乘除
+        Scanner in=new Scanner(System.in);
+        int result;
+        int a=in.nextInt();
+        int b=in.nextInt();
+        char num=in.next().charAt(0);
+        switch (num){
+            case '+':
+                System.out.println(a+b);
+                break;
+            case '-':
+                System.out.println(a-b);
+                break;
+            case'*':
+                System.out.println(a*b);
+                break;
+            case'/':
+                System.out.println(a/b);
+                break;
+        }
+         */
+    }
+
+
+}
